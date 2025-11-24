c21bdbaefbe0f932c6cb074db4f0a4a85b4803c0
Sravani <sravanipanamgipalli@gmail.com>
2025-11-24 23:10:34 +0530

    Q16

---
+++ es6/Q16.js
@@ -0,0 +1,6 @@
+// a) Arrow function to check if number is even
+const isEven = (n) => n % 2 === 0;
+// b) Ternary operator
+const result = marks >= 35 ? "Pass" : "Fail";
+// c) Arrow function with ternary operator and default parameter
+const greet = (name) => console.log(name ? `Hello, ${name}` : "Guest");
